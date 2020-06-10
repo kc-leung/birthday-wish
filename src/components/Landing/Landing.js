d32c9eea51d3ba0e4c5248ad6b7528d4f40331f1
kc-leung <chiu-leung@Hotmail.com>
2020-06-10 17:16:40 -0400

    landing basic structure set up

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+
+import { items } from '../../data/constant';
+import video from '../../assets/test.mp4';
+
 import './Landing.scss';
 
 export default class Landing extends Component {
   render() {
     return (
       <div className="landingContainer">
-        <div className="contentContainer">landing</div>
+        <div className="contentContainer">
+          <video
+            className="video"
+            src={video}
+            autoPlay={true}
+            controls={true}
+          />
+          {items.map((data, index) => {
+            return (
+              <div className="itemContainer">
+                {data.switch ? (
+                  <div className="item" key={index}>
+                    <div className="description">{data.description}</div>
+                    <div className="image">{data.picture}</div>
+                  </div>
+                ) : (
+                  <div className="item" key={index}>
+                    <div className="image">{data.picture}</div>
+                    <div className="description">{data.description}</div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
